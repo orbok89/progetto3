9ab59862f0a6604a7701aed04412d1994d06976c
orbok <fedferrara89@gmail.com>
2022-01-06 16:02:45 +0100

    prova 2

--- progetto3/progetto3.cpp
+++ progetto3/progetto3.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 	int number;
-	cout << "please insert number";
+	cout << "please insert number ";
 	cin >> number;
 	if (number % 2 == 0) {
 		cout << "even number" << endl;
@@ -16,6 +16,17 @@ int main()
 	else {
 		cout << "odd number" << endl;
 	}
+	int lato1, lato2, lato3;
+	cout << "insert 3 number";
+	cin >> lato1 >> lato2 >> lato3;
+	if (lato1 == lato2 && lato2 == lato3) {
+		cout << "triangle equilatero"<<endl;
+	}
+	else if (lato1 != lato2 && lato2 != lato3 && lato3 != lato1) {
+		cout << "scaleno" <<endl;
+	}
+	else
+		cout << "iso" <<endl;
 	cout << "bye";
 	system("pause>0");
 }
